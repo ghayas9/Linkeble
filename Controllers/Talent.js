4bf146688bbecc6a94f35d47f89142e2d5e6bc1e
ghayas9 <ghayasudin999@gmail.com>
2022-07-26 17:35:26 +0500

    init

---
+++ Controllers/Talent.js
@@ -0,0 +1,39 @@
+const Joi = require('joi')
+
+module.exports = {
+    createServicesStepOne:async(req,res)=>{
+        const value = Joi.object({
+            title: Joi.string().mix(90).required(),
+            description: Joi.string().required(),
+            catagory:Joi.string().required(),
+            subcatagory:Joi.string().required(),
+            tags:Joi.array().required()
+        }).validate(req.body)
+        if(value.error){
+            return res.status(400).json({
+                 success: false, message:value.error.message
+            })
+        }
+        //DATABASE
+        //DATABASE
+
+    },
+    createServicesStepTwo:async(req,res)=>{
+        const value = Joi.object({
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            deliver:Joi.number().required(),
+            numberofscreen:Joi.string(),
+            revisions:Joi.number().required(),
+            price:Joi.number.required()
+        }).validate(req.body)
+        if(value.error){
+            return res.status(400).json({
+                    success: false, message:value.error.message
+            })
+        }
+
+        //DATABASE
+        //DATABASE
+    }
+}
