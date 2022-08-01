a76bd39be8dd35c60bf1ecd627997a9e2e1a60b3
ghayas9 <ghayasudin999@gmail.com>
2022-08-01 13:58:40 +0500

    01/08/2022

--- Controllers/Talent.js
+++ Controllers/Talent.js
@@ -1,5 +1,9 @@
 const Joi = require('joi')
 
+const Service = require('../Models/createServices')
+const Message = require('../Models/Message')
+const mongoose = require('mongoose')
+
 module.exports = {
     createServicesStepOne:async(req,res)=>{
         const value = Joi.object({
@@ -11,12 +15,27 @@ module.exports = {
         }).validate(req.body)
         if(value.error){
             return res.status(400).json({
-                 success: false, message:value.error.message
+                 success: false, 
+                 message:value.error.message
             })
         }
-        //DATABASE
-        //DATABASE
+        const newService = new Service(req.body)
+        newService._id = mongoose.Types.ObjectId()
 
+        try{
+            const createService = await newService.save()
+            return res.json({
+                success:true,
+                message:'created successfullly',
+                service:createService
+            })
+        }catch(err){
+            return res.status(500).json({
+                success:false,
+                message:'server issue please try again later',
+                err
+            })
+        }
     },
     createServicesStepTwo:async(req,res)=>{
         const InnerValues = Joi.object().keys({
@@ -34,11 +53,61 @@ module.exports = {
         }).validate(req.body)
         if(value.error){
             return res.status(400).json({
-                    success: false, message:value.error.message
+                    success: false, 
+                    message:value.error.message
             })
         }
 
-        //DATABASE
-        //DATABASE
+        try{
+            const updateService = await Service.updateOne({_id:req.body.id},{
+                $set:req.body
+            })
+            return res.json({
+                success:true,
+                message:'saved successfully',
+                service:updateService
+            })
+        }catch(err){
+            console.log(err);
+            return res.status(500).json({
+                success:false,
+                message:'server issue please try again later',
+                err
+            })
+        }
+    },
+    sendMessage:async(req,res)=>{
+        const value = Joi.object({
+            message: Joi.string().required(),
+            creactedfor:Joi.string().required(),
+            file: Joi.string(),
+        }).validate(req.body)
+        if(value.error){
+            return res.status(400).json({
+                 success: false, 
+                 message:value.error.message
+            })
+        }
+
+        try{
+            const NewMessage = new Message()
+            NewMessage._id=mongoose.Types.ObjectId()
+            NewMessage.message=req.body.message
+            NewMessage.createdby=req.payload._id
+            NewMessage.createdfor=req.body.createdfor
+
+            if(req.body.file){
+                NewMessage.file=req.body.file
+            }
+
+            const CreatedMessage = await NewMessage.save() 
+            return res.json({
+                success:true,
+                message:CreatedMessage
+            })
+        }catch(err){
+            console.log(err);
+        }
+        
     }
 }
