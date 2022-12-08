e333ac7cae760d408cae6d073d18e27d75a0764b
ghayas9 <ghayasudin999@gmail.com>
2022-12-08 03:37:39 -0800

    08/12/2022

--- Controllers/Email/sendOTPbyEmail.js
+++ Controllers/Email/sendOTPbyEmail.js
@@ -11,41 +11,44 @@ oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 
 module.exports = {
-    sendOTPbyEmail:new Promise(async(resolve, reject)=>{
-        try{
-            const accessToken = await oAuth2Client.getAccessToken();
-            const transport = nodemailer.createTransport({
-            service: 'gmail',
-                auth: {
-                    type: 'OAuth2',
-                    user: owner_email,
-                    clientId: CLIENT_ID,
-                    clientSecret: CLEINT_SECRET,
-                    refreshToken: REFRESH_TOKEN,
-                    accessToken: accessToken,
-                },
-            });
-            const mailOptions = {
-              from: `Linkable <${owner_email}>`,
-              to: email,
-              subject: 'Forget Password',
-              text: 'Forget Password',
-              html: `<div>
-                        <div style="color:gray;padding:25px text-align:center">
-                        </div>
-                        <h1 style='padding:25px; color:blue' >${OTP}</h1>
-                    </div>`
-            };
-    
-             await transport.sendMail(mailOptions)
-             resolve({
-                success:true,
-                message:'Email has been sent successfully'
-             })
-        }catch(err){
-            reject({
-                success:false,
-                err
-            })
-        }
-    })}
+    sendOTPbyEmail: (OTP,email)=>{
+        return new Promise(async(resolve, reject)=>{
+            try{
+                const accessToken = await oAuth2Client.getAccessToken();
+                const transport = nodemailer.createTransport({
+                service: 'gmail',
+                    auth: {
+                        type: 'OAuth2',
+                        user: owner_email,
+                        clientId: CLIENT_ID,
+                        clientSecret: CLEINT_SECRET,
+                        refreshToken: REFRESH_TOKEN,
+                        accessToken: accessToken,
+                    },
+                });
+                const mailOptions = {
+                  from: `Linkable <${owner_email}>`,
+                  to: email,
+                  subject: 'Forget Password',
+                  text: 'Forget Password',
+                  html: `<div>
+                            <div style="color:gray;padding:25px text-align:center">
+                            </div>
+                            <h1 style='padding:25px; color:blue' >${OTP}</h1>
+                        </div>`
+                };
+        
+                 await transport.sendMail(mailOptions)
+                 resolve({
+                    success:true,
+                    message:'Email has been sent successfully'
+                 })
+            }catch(err){
+                reject({
+                    success:false,
+                    err
+                })
+            }
+        })
+    }
+}
