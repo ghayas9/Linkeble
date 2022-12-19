d396adbc0e3b126236a5e888156aa1d731cc10c7
ghayas9 <ghayasudin999@gmail.com>
2022-12-19 21:35:00 +0500

    error fix

--- Routes/Buyer.js
+++ Routes/Buyer.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { Payments } = require('../Controllers/Orders/Payments')
+const { payments } = require('../Controllers/Orders/Payments')
 const { placeOrderStepOne, PlaceOrderStepThree } = require('../Controllers/Orders/PlaceOrder')
 const route = express.Router()
 
@@ -7,7 +7,7 @@ const route = express.Router()
 const {auth }= require('../Middleware/auth')
 
 route.post('/place/order/step/one',auth,placeOrderStepOne)
-route.post('/place/order/step/two',auth,Payments)
+route.post('/place/order/step/two',auth,payments)
 route.post('/place/order/step/three/:id',auth,PlaceOrderStepThree)
 
 module.exports= route
